1e526bd16eabdb7509946609b73e576f6cddfdce
Yazar28 <chrisyazar28@gmail.com>
2025-12-03 09:38:27 -0600

    Corrección de errores en la creación de carpetas

---
+++ UniversalCompressor/Algorithms/LZ78Algorithm.cs
@@ -0,0 +1,17 @@
+﻿namespace UniversalCompressor.Algorithms
+{
+    public class LZ78Algorithm : ICompressionAlgorithm
+    {
+        public string Name => "LZ78";
+
+        public void Compress(string inputPath, string outputPath)
+        {
+            // TODO: Implementar LZ78 compresión
+        }
+
+        public void Decompress(string inputPath, string outputPath)
+        {
+            // TODO: Implementar LZ78 descompresión
+        }
+    }
+}
